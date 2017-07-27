8f6395d5d8374d4285056b47deeb36a530d3ec7d
Mark Anderson <udacity.com@ma7.org>
2017-07-27 16:01:44 -0500

    feat: Navigate search page to main page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 
 class SearchPage extends Component {
@@ -16,12 +17,9 @@ class SearchPage extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a
-            className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}
-          >
+          <Link className="close-search" to="/">
             Close
-          </a>
+          </Link>
           <div className="search-books-input-wrapper">
             {/* 
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
