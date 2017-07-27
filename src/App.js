0bed4c2d469536c041427856706f8ce7b0942240
Mark Anderson <udacity.com@ma7.org>
2017-07-27 17:53:01 -0500

    refactor: Create BookCover component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,49 @@ import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: [
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&source=gbs_api',
+      },
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&source=gbs_api',
+      },
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&source=gbs_api',
+      },
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&source=gbs_api',
+      },
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&source=gbs_api',
+      },
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&source=gbs_api',
+      },
+      {
+        previewLink:
+          'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&source=gbs_api',
+      },
+    ],
+  };
 
   render() {
+    const { books } = this.state;
+
     return (
       <div className="app">
-        <Route exact path="/" render={() => <MainPage />} />
+        <Route
+          exact
+          path="/"
+          render={() => <MainPage books={this.state.books} />}
+        />
         <Route path="/search" component={() => <SearchPage />} />
       </div>
     );
---
+++ src/BookCover.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const BookCover = props => {
+  const { previewLink } = props;
+
+  const backgroundImage = `url("${previewLink}&img=1&zoom=1")`;
+  return (
+    <div
+      className="book-cover"
+      style={{
+        width: 128,
+        height: 188,
+        backgroundImage: backgroundImage,
+      }}
+    />
+  );
+};
+
+export default BookCover;
