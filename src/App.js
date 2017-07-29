a6e759273f15187f4b01b45d3c0bd2439e801b9c
Mark Anderson <udacity.com@ma7.org>
2017-07-28 19:31:53 -0500

    fix: Match search result dropdown with GetAll shelves

--- src/App.js
+++ src/App.js
@@ -11,14 +11,29 @@ class BooksApp extends React.Component {
     books: [],
     searchResults: [],
     lastQuery: '',
+    lookupBooks: {},
+  };
+
+  // BooksAPI returns books with shelf values that do not
+  //  match GetAll.  Some books in "My"Reads 'Currently Reading'
+  //  return as none, other show up with a value of shelf set
+  //  but should be 'none'
+  improveResult = book => {
+    const idx = this.state.lookupBooks[book.id];
+    if (idx) {
+      return { ...book, shelf: this.state.books[idx].shelf };
+    } else {
+      return { ...book, shelf: 'none' };
+    }
   };
 
   loadSearchResults = query => {
+    this.setState({ lastQuery: query });
     BooksAPI.search(query, 5).then(result => {
       if (result && !result.error) {
+        let betterResults = result.map(this.improveResult);
         this.setState({
-          searchResults: result.sort(this.bookSort),
-          lastQuery: query,
+          searchResults: betterResults.sort(this.bookSort),
         });
       } else {
         this.setState({ searchResults: [], lastQuery: query });
@@ -45,6 +60,22 @@ class BooksApp extends React.Component {
     }));
   };
 
+  getAllToState = () => {
+    BooksAPI.getAll().then(books => {
+      var lookupBooks = {};
+      books.forEach((element, index) => {
+        lookupBooks[element.id] = index;
+      });
+      console.log('[getAllToState');
+      console.log(lookupBooks);
+      console.log(']getAllToState');
+      this.setState({
+        books: books.sort(this.bookSort),
+        lookupBooks,
+      });
+    });
+  };
+
   // decided agains doing a lot of computation and just call getAll to keep in sync.
   //   local change will be quick for user
   //   getAll should change nothing, React will be smart with the virtualDOM
@@ -55,29 +86,17 @@ class BooksApp extends React.Component {
 
     BooksAPI.update(changingBook, shelf)
       .then(response => {
-        BooksAPI.getAll().then(books => {
-          this.setState({
-            books: books.sort(this.bookSort),
-          });
-        });
+        this.getAllToState();
       })
       .catch(e => {
         console.log('error:');
         console.log(e);
-        BooksAPI.getAll().then(books => {
-          this.setState({
-            books: books.sort(this.bookSort),
-          });
-        });
+        this.getAllToState();
       });
   };
 
   componentDidMount() {
-    BooksAPI.getAll().then(books => {
-      this.setState({
-        books: books.sort(this.bookSort),
-      });
-    });
+    this.getAllToState();
   }
 
   render() {
